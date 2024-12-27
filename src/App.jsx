98182f51a57492d5db070a86fe90f3eeb5f8ff68
Brice VINCENT <contact@alsacreaweb.fr>
2024-12-28 00:36:24 +0100

    add import on app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useContext } from "react";
 import { PlayingContext } from "./Context";
 import MainLayout from "./layouts/MainLayout";
+import DefineStartModePlayGame from "./views/DefineStartModePlayGame";
+import InputPlayerName from "./views/InputPlayerName";
+import InputGameId from "./views/InputGameId";
+import WaitingEnemy from "./views/WaitingEnemy";
 
 function App() {
   const { machineState } = useContext(PlayingContext);
